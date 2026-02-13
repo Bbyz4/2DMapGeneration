79b5f19d691c0e985871a25aa57aeadf4d035c9d
Bbyz <bbyzdev@gmail.com>
2026-02-13 21:28:32 +0100

    Added some placeholder popup, imported textmeshpro

--- 2DMapGeneration/Assets/Scripts/MapGen/MainScene/UILoader.cs
+++ 2DMapGeneration/Assets/Scripts/MapGen/MainScene/UILoader.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class UILoader : MonoBehaviour
 {
     [SerializeField] private BiomeGeneratorPopup biomeGeneratorPopup;
+    [SerializeField] private MountainGeneratorPopup mountainGeneratorPopup;
+    [SerializeField] private ObjectGeneratorPopup objectGeneratorPopup;
 
     void Awake()
     {
-        
+        biomeGeneratorPopup.gameObject.SetActive(true);
     }
 }
